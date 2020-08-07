79fdf7e47378ebeab1d59e4557457674cceb51fd
linchaoyong <lincy18845091124@aliyun.com>
2020-08-07 15:40:12 +0800

    第二次修改，从GitHub导后的第一次修改

--- src/main/java/gitdemo/GitTest.java
+++ src/main/java/gitdemo/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("新的STS的Git项目");
 		System.out.println("第一次修改");
+		System.out.println("第二次修改，从GitHub导后的第一次修改");
 	}
 
 }
